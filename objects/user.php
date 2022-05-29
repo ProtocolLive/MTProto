ba27d06dc19589484af7afa42a89104145a76b75
Protocol Corporation <protocollive@outlook.com>
2022-05-29 12:07:23 -0300

    Objetos faltando

---
+++ objects/user.php
@@ -0,0 +1,22 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive
+//2022.05.28.00
+
+class User{
+  public readonly int $Id;
+  public readonly string $Name;
+  public readonly string $NameLast;
+
+  public function __construct(string $Data){
+    
+  }
+}
+
+class UserNone{
+  public readonly int $Id;
+
+  public function __construct(string $Data){
+    
+  }
+}
