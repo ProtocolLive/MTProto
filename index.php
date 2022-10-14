ced73aeb95890ece4cf49e331f096789a75ef0a6
Protocol Corporation <protocollive@outlook.com>
2022-10-14 16:10:17 -0300

    Removed the "clear"

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ use ProtocolLive\Mtproto\Transport;
 
 require(__DIR__ . '/php.php');
 require(__DIR__ . '/vendor/autoload.php');
-echo "\e[H\e[J";
 
 $a = new Mtproto(Transport::Abridged, true);
 $a->PqRequire(md5(uniqid()));
