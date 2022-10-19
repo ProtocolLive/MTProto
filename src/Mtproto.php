e73d430f86160efee2cba4a8691d5a315f5ce0a7
Protocol Corporation <protocollive@outlook.com>
2022-10-19 13:59:13 -0300

    Sending DhRequire, dump messages, fix some vars, fix RsaPad param

--- src/Mtproto.php
+++ src/Mtproto.php
@@ -1,10 +1,9 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive
-//2022.10.18.03
+//2022.10.19.00
 
 namespace ProtocolLive\Mtproto;
-
 use Exception;
 use ProtocolLive\Mtproto\Servers\Servers;
 
@@ -36,31 +35,47 @@ class Mtproto extends Basics{
     $count = str_pad($count, 8, 0, STR_PAD_LEFT);
     $count = self::InvertEndian($count);
     $Payload = $auth_key_id . $message_id . $count . $method . $nonce;
-    $this->Send($Payload, $Dump);
+    $this->Send($Payload, 'PqRequire');
 
     //https://core.tlgr.org/mtproto/samples-auth_key#2-a-response-from-the-server-has-been-received-with-the-following-content
-    $return = $this->Read($Dump);
-    $return = new PqResponse($return);
+    $temp = $this->Read('PqResponse');
+    $pqresponse = new PqResponse($temp);
 
     //https://core.tlgr.org/mtproto/samples-auth_key#3-pq--17ed48941a08f981-decomposed-into-2-prime-cofactors
-    $pq = self::FactorWolfram($return->Pq, $Dump);
+    $pq = self::FactorWolfram($pqresponse->Pq, $Dump);
 
     //https://core.tlgr.org/mtproto/samples-auth_key#4-encrypted-data-generation
     $data = self::InvertEndian(Methods::PqInnerData->value);
-    $data .= '08' . $return->Pq . '000000';
+    $data .= '08' . $pqresponse->Pq . '000000';
     $data .= '04' . $pq->P . '000000';
     $data .= '04' . $pq->Q . '000000';
     $data .= $nonce;
-    $data .= $return->NonceServer;
+    $data .= $pqresponse->NonceServer;
     $data .= bin2hex(random_bytes(32));
-    $data_hash = sha1($data);
-    $return = self::RsaPad($data, $this->Server::PubKey);
+    $data_hash = sha1(hex2bin($data));
+    $rsapad = self::RsaPad($data, $this->Server);
 
     //https://core.tlgr.org/mtproto/samples-auth_key#request-to-start-diffie-hellman-key-exchange
     $message_id = microtime(true) * pow(2, 32);
     $message_id = dechex($message_id);
     $message_id = self::InvertEndian($message_id);
-
-    die();
+    $payload = self::InvertEndian(Methods::DhRequire->value);
+    $payload .= $nonce;
+    $payload .= $pqresponse->NonceServer;
+    $payload .= '04' . $pq->P . '000000';
+    $payload .= '04' . $pq->Q . '000000';
+    $payload .= $pqresponse->Fingerprint;
+    $count = strlen($rsapad->EncryptedData) / 2;
+    $count = dechex($count);
+    $count = 'fe00' . self::SafeHex($count);
+    $payload .= $count . $rsapad->EncryptedData;
+    $count = strlen($payload) / 2;
+    $count = dechex($count);
+    $count = '0000' . self::SafeHex($count);
+    $count = self::InvertEndian($count);
+    $payload = $auth_key_id . $message_id . $count . $payload;
+    self::HexDebug($payload);
+    self::Send($payload, 'DhRequire');
+    self::Read('DhServer');
   }
 }
